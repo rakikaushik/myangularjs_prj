d72ff22a2c8aab5451baa18698856488d7769dbd
rakikaushik <rakikaushik@gmail.com>
2021-05-17 13:39:15 +0530

    Assignment One is completed

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,12 +1,39 @@
-(function () {
-'use strict';
+(function() {
+  "use strict";
 
-var x = "hello";
+  angular.module("LunchCheck", []).controller("LunchCheckController", LunchCheckController);
 
-angular.module('myFirstApp', [])
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController($scope) {
+    $scope.itemString = "";
+    $scope.nbOfItems = 0;
+    $scope.resultString;
+    // $scope.resultClass;
+    $scope.resultOk = null;
 
-.controller('MyFirstController', function () {
+    $scope.checkItems = function() {
+      if($scope.itemString.length > 0) {
+        console.log("Items entered: " + $scope.itemString);
+        var itemArray = $scope.itemString.split(",");
+        console.log("Items as array: " + itemArray);
 
-});
+        $scope.resultString = checkItemCount(itemArray);
+        $scope.resultOk = true;
+      }
+      else
+      {
+        $scope.resultString = "Please enter data first";
+        $scope.resultOk = false;
+      }
+    };
+  }
 
+  function checkItemCount(items) {
+    var itemCount = items.length;
+    console.log("Number of items entered: " + itemCount);
+    if(itemCount < 4)
+      return "Enjoy!";
+    else
+      return "Too much!";
+  }
 })();
